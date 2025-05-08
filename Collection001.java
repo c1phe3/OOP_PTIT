eebb5cb9ac99a63ef8b75c381a5505e2817dbc3f
c1ph3 <chiuai000@gmail.com>
2025-05-08 14:42:36 +0700

    update Collection001

---
+++ Collection001.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Warehouse {
+    private String product;
+    private int price;
+    private int stock;
+
+    public addProduct(String product, int price, int stock) {
+        this.product = product;
+        this.price = price;
+        this.stock = stock;
+    }
+    
+    public price(String product) {
+        return price;
+    }
+
+    
+}
+
+public class Collection001{
+    public class void main(String [] args){
+        Warehouse wareHouse = new wareHouse();
+    }
+}
+
+
